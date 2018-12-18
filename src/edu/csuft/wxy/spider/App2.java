b3958019c205c695e43f5c16bdc3664f24495edd
Yang-Awa <45841258+Yang-Awa@users.noreply.github.com>
2018-12-18 08:04:32 +0800

    抓取数据

--- src/edu/csuft/wxy/spider/App2.java
+++ src/edu/csuft/wxy/spider/App2.java
@@ -13,24 +13,26 @@ import org.jsoup.select.Elements;
  * @author 19812
  *
  */
-public class App {
+public class App2 {
 
-	// alt + /  快捷键     先打main
+	/**1、先准备路径
+	 *2、JSoup
+	 *3、获得网页中的内容（DOM 树）
+	 * @param args
+	 */
  	
 	public static void main(String[] args) {
 		
 		//目标 URL
-		String url="https://movie.douban.com/top250";
+		String url="https://movie.douban.com/chart";
 		
 		//使用JSOUP 抓取文档
-		//alt + /
+		//alt +/
 		try {
 			Document doc = Jsoup.connect(url).get();
-//			集合多个
-			Elements es =doc.select(".grid_view .item");
+			
+			Elements es =doc.select(".indent .item");
 //			class前需要加“.”
-//			集合一个
-//			Elements e =doc.selectFirst("");
 			System.out.println(es.size());
 			
 			//创建一个存储影片的列表
@@ -39,28 +41,28 @@ public class App {
 			for(Element e :es) {
 				Film f = new Film();
 ////				每一部影片
-//			Element t=e.select(".title").first();
+			Element t=e.select(".pl2 a").first();
 //			String num=e.select(".star span").last().text();
-//				System.out.println(t.text()+","+num);
+				System.out.println(t.text());
 				
-				f.id=Integer.parseInt(e.select(".pic em").first().text());
-				f.poster = e.select("img").first().attr("src");
-				f.info = e.select(".bd p").first().text();
-				f.title = e.select(".title").first().text();
-				f.rating =Double.parseDouble( e.select(".rating_num").first().text());
-				String num =e.select(".star span").last().text();
-				f.num =Integer.parseInt(num.substring(0, num.length()-3));
-				f.quote = e.select(".inq").first().text();
-				
-				System.out.println(f);
-				list.add(f);
+//				f.id=Integer.parseInt(e.select(".pic em").first().text());
+//				f.poster = e.select("img").first().attr("src");
+//				f.info = e.select(".bd p").first().text();
+//				f.title = e.select(".title").first().text();
+//				f.rating =Double.parseDouble( e.select(".rating_num").first().text());
+//				String num =e.select(".star span").last().text();
+//				f.num =Integer.parseInt(num.substring(0, num.length()-3));
+//				f.quote = e.select(".inq").first().text();
+//				
+//				System.out.println(f);
+//				list.add(f);
 			}
 			
-//			String title=doc.title();
+			String title=doc.title();
 //			String html=doc.html();
 //			String data=doc.data();
 //			
-//			System.out.println(title);
+			System.out.println(title);
 //			System.out.println(html);
 //			System.out.println(data);
 		} catch (IOException e) {
